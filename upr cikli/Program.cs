f27db0adc73c339b61766afcaffd1a020da33e6b
hristo222h <hristomirhristov09@gmail.com>
2024-12-13 09:07:52 +0200

    Add files via upload

---
+++ upr cikli/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upr_cikli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n =");
+            int n = int.Parse(Console.ReadLine());
+            for (var i = 1; i <= n; i++)
+            {
+                Console.Write(" * ");
+                for (var c = 1; c < n; c++)
+                {
+                   Console.Write(" * ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ upr cikli/upr cikli1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upr_cikli1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var n = int.Parse(Console.ReadLine());
+            for (var a = n; a >= 1; a--)
+            {
+                Console.Write("$");
+                for (var b = 1; b < a; b++)
+                {
+                    Console.Write(" $");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
