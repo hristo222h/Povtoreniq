e427ae30968fc27c8b4ef37c98a64ca77c951970
hristo222h <hristomirhristov09@gmail.com>
2024-12-13 09:04:54 +0200

    Add files via upload

---
+++ faktoriel/faktoriel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faktoriel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            int p = 1;
+            while(n > 1)
+            {
+                p = p * n;
+                n--;
+            }
+            Console.WriteLine("p = " + p);
+        }
+    }
+}
---
+++ faktoriel/sumirane na chisla/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumirane_na_chisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var s = 0;
+            do
+            {
+                s = s + (n % 10);
+                n = n / 10;
+            } while (n > 0);
+            int u = s % 100/ 10;
+            int i = s % 10;
+            int o = s / 10;
+            Console.WriteLine("sum of digits: {0}", s);
+            if (n > 99)
+            {
+                Console.WriteLine("stotici " + o);
+            }
+            Console.WriteLine("desetici " + u);
+            Console.WriteLine("edinici " + i);
+
+        }
+    }
+}
